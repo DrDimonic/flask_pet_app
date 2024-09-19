e91e10c2ce5528ab10a93f87e3c16bdd80bd7335
Dominic Cork <dcork1@kent.edu>
2024-09-18 23:31:39 -0400

    Testing

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ from models import db, Pet
 from forms import PetForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/pets.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SECRET_KEY'] = '8f81176d4b15a77cb8cbf472903c610436346e3af4d4b98c'
 db.init_app(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()  # Create tables in the in-memory databas
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = PetForm()
     if form.validate_on_submit():
-        pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect(url_for('index'))
-    pets = Pet.query.all()
+        # Mock database insertion logic
+        pass
+    # Mock list of pets
+    pets = [
+        {'name': 'Buddy', 'age': 3, 'type': 'Dog'},
+        {'name': 'Whiskers', 'age': 2, 'type': 'Cat'}
+    ]
     return render_template('view_pets.html', form=form, pets=pets)
 
 if __name__ == '__main__':
